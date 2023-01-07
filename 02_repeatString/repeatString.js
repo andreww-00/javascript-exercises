99c0f6f93a3751d1333ce848d66b7f220b5ff16c
Andrew <a.lucas.webster@gmail.com>
2023-01-06 19:33:02 -0500

    Answer to exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
-
+const repeatString = function (str, iteration) {
+  let string = str;
+  let num = iteration;
+  let i = 1;
+  if (num < 0) {
+    return "ERROR";
+  } else if (num == 0) {
+    return "";
+  } else if (str == "") {
+    return "";
+  } else {
+    while (i < num) {
+      i++;
+      string = string + "hey";
+    }
+    return string;
+  }
 };
-
 // Do not edit below this line
 module.exports = repeatString;
