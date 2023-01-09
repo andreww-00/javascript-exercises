02531a3daea2ffc1835d8e2e738835302bd44118
Andrew <a.lucas.webster@gmail.com>
2023-01-09 16:49:53 -0500

    Dirty solution to problem 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, argument) {
+// this is before I knew indexOf existed I kinda created
+//it myself tho
+// let array = array;
+// for (i=0; i < array.length(); i++) {
+//     if (deleted = array[i]) {
 
+//     }
+// }
+let indexRemoval = array.indexOf(argument)
+let newArray = array.splice(0, 1);
+return newArray;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+  let sum = 0;
+  if (end >= start && start>=0 && end>=0 && Number.isInteger(start) && Number.isInteger(end)) {
+    for (i = start; i <= end; i++) {
+      sum += i;
+    }
+  } else if (start >= end && start>=0 && end>=0 && Number.isInteger(start) && Number.isInteger(end)) {
+    for (i = end; i <= start; i++) {
+      sum += i;
+    }
+  } else {
+    return "ERROR";
+  }
+  return sum;
 };
 
 // Do not edit below this line
