1f954fdc62db6c926432c11a07a9c08181efa728
Andrew <a.lucas.webster@gmail.com>
2023-03-07 18:01:49 -0500

    solution to calucaltor

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(x,y) {
+	let sum = x + y;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	let sub = x - y;
+  return sub;
 };
 
-const sum = function() {
-	
+const sum = function(result) {
+	return result.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(result) {
+ return result.reduce((total, current) => total * current, 1);
 };
 
-const power = function() {
-	
+const power = function(x,a) {
+	return Math.pow(x,a);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	// x*(x-1)*(x-2)..*1
+  let fact = 1;
+  if (x===0) {
+    return 1;
+  } else {
+    while (x>0) {
+      fact = fact * x;
+      x = x-1;
+    }
+    return fact;
+  }
+  //could be simplified with a for loop
 };
 
 // Do not edit below this line
