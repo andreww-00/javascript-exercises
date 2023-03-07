f6613847e2bea7f3d7b37de5312f6243927627f8
Andrew <a.lucas.webster@gmail.com>
2023-03-07 18:46:39 -0500

    solution to palindromes 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
+const palindromes = function (string) {
+// returns true if string === string reverse
+//reverse string then comparasion then return
+let reversed =  string.toLowerCase().replace(/[^a-z]/g, "");
+return reversed.split("").reverse().join("") == reversed;
 
 };
-
 // Do not edit below this line
 module.exports = palindromes;
